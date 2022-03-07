e4b1269dc83f7a88ef3decde72256fac5cbc2851
BambooSword <dengliming@tngdigital.com.my>
2022-03-07 15:36:31 +0800

    fix: change order and add 13-parameters

--- type-challenges/easy/10-Length_of_Tuple/template.ts
+++ type-challenges/easy/10-Length_of_Tuple/template.ts
@@ -1,4 +1,4 @@
-type Length<T extends readonly any[]> = T['length']
+export type Length<T extends readonly any[]> = T['length']
 
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 
